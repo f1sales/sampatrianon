42f8b9c11e5e8aad0e4f54fe9af1b15fc44e508e
Marcio Klepacz <marcioklepacz@gmail.com>
2019-08-29 11:45:48 -0300

    Add peugeot

--- spec/hooks_spec.rb
+++ spec/hooks_spec.rb
@@ -3,6 +3,29 @@ require "f1sales_custom/hooks"
 
 RSpec.describe F1SalesCustom::Hooks::Lead do
 
+  context 'when is peugeot' do
+    context 'when is to lapa' do
+      let(:source) do
+        source = OpenStruct.new
+        source.name = 'Facebook - Peugeot Trianon'
+        source
+      end
+
+      let(:lead) do
+        lead = OpenStruct.new
+        lead.message = 'loja: Lapa: contato: 1198899888'
+        lead.source = source
+
+        lead
+      end
+
+      it 'sets Lapa store source' do
+        expect(described_class.switch_source(lead)).to eq(source.name + described_class.source[:facebook_lapa])
+      end
+
+    end
+  end
+
   context 'when is citroen' do
 
     context 'when is to lapa' do
